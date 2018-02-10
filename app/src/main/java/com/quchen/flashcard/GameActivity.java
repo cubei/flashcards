8d04daed6524f0eb1217e905b8a0e0e9b5624a7b
lharmsen <0quchen@googlemail.com>
2018-02-10 17:58:04 +0100

    Switched game view to fragement

--- app/src/main/java/com/quchen/flashcard/GameActivity.java
+++ app/src/main/java/com/quchen/flashcard/GameActivity.java
@@ -1,8 +1,11 @@
 package com.quchen.flashcard;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -20,110 +23,17 @@ public class GameActivity extends AppCompatActivity {
     public static final String KEY_TIME_TRIAL_RESET = "timeTrialReset";
     public static final String KEY_TIME_PER_ITEMS = "timePerItem";
 
-    private List<QuestionItem> questionItems = new ArrayList<>();
-    private List<QuestionResult> questionResults = new ArrayList<>();
+    private FrameLayout frameLayout;
+
     private int side;
     private int numberOfDesireditems;
     private boolean isTimeTrial;
     private boolean isTimeTrialReset;
     private int timePerItem;
 
-    private List<TextView> answerTextViews = new ArrayList<>();
-    private TextView timeTextView;
-    private TextView progressTextView;
-    private TextView listNameTextView;
-    private TextView questionTextView;
-    private TextView questionSideLabel;
-    private TextView answerSideLabel;
-    private TextView correctCountTextView;
-    private TextView wrongCountTextView;
+    private QuestionFragment questionFragment;
 
-    private int questionCount = 0;
-    private int wrongAnswerCount = 0;
-    private int correctAnswerCount = 0;
 
-    private View.OnClickListener answerOnClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            TextView answerTextView = (TextView) view;
-            QuestionItem questionItem = questionItems.get(questionCount);
-            QuestionResult questionResult = new QuestionResult(questionItem.question, questionItem.rightAnswer, answerTextView.getText().toString());
-            questionResults.add(questionResult);
-
-            if(questionResult.isAnswerCorrect()) {
-                correctAnswerCount++;
-            } else {
-                wrongAnswerCount++;
-            }
-
-            setUpNextQuestion();
-        }
-    };
-
-    private void assignViews() {
-        answerTextViews.add((TextView) findViewById(R.id.tv_answer1));
-        answerTextViews.add((TextView) findViewById(R.id.tv_answer2));
-        answerTextViews.add((TextView) findViewById(R.id.tv_answer3));
-        answerTextViews.add((TextView) findViewById(R.id.tv_answer4));
-
-        timeTextView = findViewById(R.id.tv_time);
-        progressTextView = findViewById(R.id.tv_progress);
-        listNameTextView = findViewById(R.id.tv_listName);
-        questionTextView = findViewById(R.id.tv_question);
-        questionSideLabel = findViewById(R.id.tv_questionSide);
-        answerSideLabel = findViewById(R.id.tv_guessSide);
-        correctCountTextView = findViewById(R.id.tv_correctAnswerCount);
-        wrongCountTextView = findViewById(R.id.tv_wrongAnswerCount);
-    }
-
-    private void setUpViews() {
-        correctCountTextView.setText(String.format("%d", 0));
-        wrongCountTextView.setText(String.format("%d", 0));
-
-        updateViews();
-    }
-
-    private void updateViews() {
-        progressTextView.setText(String.format("%d / %d", questionCount, numberOfDesireditems));
-        correctCountTextView.setText(String.format("%d", correctAnswerCount));
-        wrongCountTextView.setText(String.format("%d", wrongAnswerCount));
-    }
-
-    private void setQuestionItem(QuestionItem questionItem) {
-        listNameTextView.setText(questionItem.listFilePath);
-        questionTextView.setText(questionItem.question);
-        questionSideLabel.setText(questionItem.questionHeader);
-        answerSideLabel.setText(questionItem.answerHeader);
-
-        List<String> answers = new ArrayList<>();
-        answers.add(questionItem.rightAnswer);
-
-        List<String> wrongAnswers = new ArrayList<>(questionItem.wrongAnswers);
-        for(int i=0; i<3; i++) {
-            String wrongAnswer = wrongAnswers.get((int) (Math.random() * wrongAnswers.size()));
-            wrongAnswers.remove(wrongAnswer);
-            answers.add(wrongAnswer);
-        }
-
-        Collections.shuffle(answers);
-
-        for(int i=0; i<4; i++) {
-            TextView tv = answerTextViews.get(i);
-            tv.setText(answers.get(i));
-            tv.setOnClickListener(answerOnClick);
-        }
-    }
-
-    private void setUpNextQuestion() {
-        questionCount++;
-
-        if(questionCount == numberOfDesireditems) {
-            finish();
-        } else {
-            setQuestionItem(questionItems.get(questionCount));
-            updateViews();
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -138,6 +48,7 @@ public class GameActivity extends AppCompatActivity {
 
         String[] listFiles = getIntent().getStringArrayExtra(KEY_FILE_LIST);
 
+        List<QuestionItem> questionItems = new ArrayList<>();
         for(String listFile: listFiles) {
             ListItem listItem = new ListItem(listFile);
             questionItems.addAll(QuestionItem.getQuestionItemList(listItem));
@@ -145,9 +56,11 @@ public class GameActivity extends AppCompatActivity {
 
         numberOfDesireditems = Math.min(numberOfDesireditems, questionItems.size());
 
-        assignViews();
-        setUpViews();
-
-        setQuestionItem(questionItems.get(questionCount));
+        questionFragment = QuestionFragment.newInstance(numberOfDesireditems, questionItems);
+        frameLayout = findViewById(R.id.frameLayout);
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.frameLayout, questionFragment);
+        transaction.commit();
     }
 }
--- app/src/main/java/com/quchen/flashcard/QuestionFragment.java
+++ app/src/main/java/com/quchen/flashcard/QuestionFragment.java
@@ -1,108 +1,142 @@
 package com.quchen.flashcard;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link QuestionFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link QuestionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class QuestionFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private List<QuestionItem> questionItems = new ArrayList<>();
+    private List<QuestionResult> questionResults = new ArrayList<>();
 
-    private OnFragmentInteractionListener mListener;
+    private int numberOfDesiredQuestions;
+    private int questionCount = 0;
+    private int wrongAnswerCount = 0;
+    private int correctAnswerCount = 0;
+
+    private List<TextView> answerTextViews = new ArrayList<>();
+    private TextView timeTextView;
+    private TextView progressTextView;
+    private TextView listNameTextView;
+    private TextView questionTextView;
+    private TextView questionSideLabel;
+    private TextView answerSideLabel;
+    private TextView correctCountTextView;
+    private TextView wrongCountTextView;
+
+    private void assignViews() {
+        answerTextViews.add((TextView) getView().findViewById(R.id.tv_answer1));
+        answerTextViews.add((TextView) getView().findViewById(R.id.tv_answer2));
+        answerTextViews.add((TextView) getView().findViewById(R.id.tv_answer3));
+        answerTextViews.add((TextView) getView().findViewById(R.id.tv_answer4));
+
+        timeTextView = getView().findViewById(R.id.tv_time);
+        progressTextView = getView().findViewById(R.id.tv_progress);
+        listNameTextView = getView().findViewById(R.id.tv_listName);
+        questionTextView = getView().findViewById(R.id.tv_question);
+        questionSideLabel = getView().findViewById(R.id.tv_questionSide);
+        answerSideLabel = getView().findViewById(R.id.tv_guessSide);
+        correctCountTextView = getView().findViewById(R.id.tv_correctAnswerCount);
+        wrongCountTextView = getView().findViewById(R.id.tv_wrongAnswerCount);
+    }
+
+    private void setUpViews() {
+        setQuestionItem(questionItems.get(0));
+        for(TextView tv: answerTextViews) {
+            tv.setOnClickListener(answerOnClick);
+        }
+        updateViews();
+    }
+
+    private void updateViews() {
+        progressTextView.setText(String.format("%d / %d", questionCount, numberOfDesiredQuestions));
+        correctCountTextView.setText(String.format("%d", correctAnswerCount));
+        wrongCountTextView.setText(String.format("%d", wrongAnswerCount));
+    }
+
+    private View.OnClickListener answerOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            TextView answerTextView = (TextView) view;
+            QuestionItem questionItem = questionItems.get(questionCount);
+            QuestionResult questionResult = new QuestionResult(questionItem.question, questionItem.rightAnswer, answerTextView.getText().toString());
+            questionResults.add(questionResult);
+
+            if(questionResult.isAnswerCorrect()) {
+                correctAnswerCount++;
+            } else {
+                wrongAnswerCount++;
+            }
+
+            setUpNextQuestion();
+        }
+    };
+
+    private void setQuestionItem(QuestionItem questionItem) {
+        listNameTextView.setText(questionItem.listFilePath);
+        questionTextView.setText(questionItem.question);
+        questionSideLabel.setText(questionItem.questionHeader);
+        answerSideLabel.setText(questionItem.answerHeader);
+
+        List<String> answers = new ArrayList<>();
+        answers.add(questionItem.rightAnswer);
+
+        List<String> wrongAnswers = new ArrayList<>(questionItem.wrongAnswers);
+        for(int i=0; i<3; i++) {
+            String wrongAnswer = wrongAnswers.get((int) (Math.random() * wrongAnswers.size()));
+            wrongAnswers.remove(wrongAnswer);
+            answers.add(wrongAnswer);
+        }
+
+        Collections.shuffle(answers);
+
+        for(int i=0; i<4; i++) {
+            TextView tv = answerTextViews.get(i);
+            tv.setText(answers.get(i));
+        }
+    }
+
+    private void setUpNextQuestion() {
+        questionCount++;
+
+        if(questionCount == numberOfDesiredQuestions) {
+            this.getActivity().finish();
+        } else {
+            setQuestionItem(questionItems.get(questionCount));
+            updateViews();
+        }
+    }
 
     public QuestionFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment QuestionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static QuestionFragment newInstance(String param1, String param2) {
+    public static QuestionFragment newInstance(int numberOfDesiredQuestions, List<QuestionItem> questionItems) {
         QuestionFragment fragment = new QuestionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+        fragment.numberOfDesiredQuestions = numberOfDesiredQuestions;
+        fragment.questionItems = questionItems;
         return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_question, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        assignViews();
+        setUpViews();
     }
 }
