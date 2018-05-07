a57455ba7d98504890df7ac218afe23f8af42bfa
lharmsen <0quchen@googlemail.com>
2018-05-07 21:26:32 +0200

    Added minna no nihongo lection 6 extra food vocabulary

--- app/src/main/java/com/quchen/flashcard/MainActivity.java
+++ app/src/main/java/com/quchen/flashcard/MainActivity.java
@@ -156,6 +156,7 @@ public class MainActivity extends AppCompatActivity {
         copyFileFromResource(R.raw.japanese_lektion4, "Minna no Nihongo","Lektion 4.csv");
         copyFileFromResource(R.raw.japanese_lektion5, "Minna no Nihongo","Lektion 5.csv");
         copyFileFromResource(R.raw.japanese_lektion6, "Minna no Nihongo","Lektion 6.csv");
+        copyFileFromResource(R.raw.japanese_lektion6_lebensmittel, "Minna no Nihongo","Lektion 6 Lebensmittel.csv");
         copyFileFromResource(R.raw.japanese_lektion7, "Minna no Nihongo","Lektion 7.csv");
         copyFileFromResource(R.raw.japanese_particle, "Minna no Nihongo","Particle.csv");
         copyFileFromResource(R.raw.minna_no_nihongo_kanji, "Minna no Nihongo","Kanji.csv");
